e2c45c4692526a51ab1258430d922496870837d2
yutsapova-afk <yutsapova@gmail.com>
2026-01-16 08:25:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -314,7 +314,7 @@ const DoubleSalesCalculator = () => {
       const element = pdfRef.current;
       const canvas = await html2canvas(element, { scale: 2, useCORS: true, logging: false });
       
-      const imgData = canvas.toDataURL('image/png');
+     
       const pdf = new jsPDF('p', 'mm', 'a4');
       const pdfWidth = pdf.internal.pageSize.getWidth();
       const pdfHeight = pdf.internal.pageSize.getHeight();
